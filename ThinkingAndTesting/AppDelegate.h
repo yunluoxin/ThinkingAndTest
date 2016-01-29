a48858357f21716c6dba4e97eddd87d02d0805d9
yunluoxin <hhdgw@qq.com>
2016-01-29 08:19:23 +0800

    Initial Commit

---
+++ ThinkingAndTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThinkingAndTesting
+//
+//  Created by 张小冬 on 16/1/29.
+//  Copyright © 2016年 dadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThinkingAndTesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThinkingAndTesting
+//
+//  Created by 张小冬 on 16/1/29.
+//  Copyright © 2016年 dadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
