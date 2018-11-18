b035edc8902b8a75045033b4cb87f420eb0c015a
Dadong <yunluox@qq.com>
2018-11-18 15:19:50 +0800

    Nib和init关系

--- ThinkingAndTesting/classes/Nib能否执行awake方法/NibCanAwakeViewController2ViewController.m
+++ ThinkingAndTesting/classes/Nib能否执行awake方法/NibCanAwakeViewController2ViewController.m
@@ -5,6 +5,9 @@
 //  Created by 张小冬 on 16/1/29.
 //  Copyright © 2016年 dadong. All rights reserved.
 //
+//  @attention 在调用vc的init时候，如果有VC同名的xib会自动调用，这是我们已经知道的！但是实质上
+//             调用的-initWithNibName:bundle:方法里面，不会调用-initWithCoder:，故也就不会调用awakeFromNib！！！
+//
 
 #import "NibCanAwakeViewController2ViewController.h"
 
@@ -14,8 +17,21 @@
 
 @implementation NibCanAwakeViewController2ViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    DDLog(@"%s", __func__);
+    return [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        DDLog(@"%s", __func__);
+    }
+    return self;
+}
+
 - (void)awakeFromNib
 {
+    [super awakeFromNib];
     NSLog(@"awakeFromNib");
 }
 
--- ThinkingAndTesting/classes/Nib能否执行awake方法/NibView.m
+++ ThinkingAndTesting/classes/Nib能否执行awake方法/NibView.m
@@ -4,6 +4,8 @@
 //
 //  Created by 张小冬 on 16/1/29.
 //  Copyright © 2016年 dadong. All rights reserved.
+//  在VC的Xib中添加当前view时候，awakeFromNib执行了，但是-init没有执行！
+//  说明了：-initWithCoder: 里面不会调用-init方法！所以，-initWithCoder和-init里面，都要分别初始化！
 //
 
 #import "NibView.h"
@@ -21,6 +23,7 @@
 
 - (void)awakeFromNib
 {
+    [super awakeFromNib];
     NSLog(@"awakeFromNib");
 }
 @end
