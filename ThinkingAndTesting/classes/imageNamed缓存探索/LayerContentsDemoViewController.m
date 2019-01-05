5feadff591cf92aba8d0f846542c6431a0d662b5
Dadong <yunluox@qq.com>
2019-01-06 00:17:11 +0800

    layer通过设置contentsRect => 可视范围 Demo UIImageView应用可拉伸图片和本身layer设置contentsCenter拉伸的冲突探究，结论是还是layer的contentsCenter占了结果

--- ThinkingAndTesting/classes/imageNamed缓存探索/LayerContentsDemoViewController.m
+++ ThinkingAndTesting/classes/imageNamed缓存探索/LayerContentsDemoViewController.m
@@ -17,7 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self stretchDemo];
+    [self basicDemo];
+    
+//    [self stretchDemo];
+
+//    [self stretchDemo2];
+    
+    [self visualRectDemo];
 }
 
 - (void)basicDemo {
@@ -46,11 +52,38 @@
     [self.view.layer addSublayer:layer];
     
     UIImage *image = [UIImage imageNamed:@"bg_fr_bubble_view"];
-    image = [image stretchableImageWithLeftCapWidth:20 topCapHeight:20];
+    image = [image stretchableImageWithLeftCapWidth:image.size.width - 1 topCapHeight:3];
     layer.contentsCenter = CGRectMake(0.2, 0.2, 0.2, 0.2);
     layer.contents = (id)image.CGImage;
     layer.contentsScale = [UIScreen mainScreen].scale;
     //    layer.contentsGravity = kCAGravityCenter;  // 设置成Center不会拉伸
+}
+
+/// 在这个直接用UIImageView的demo中，也证实了上面的结论！ 在对UIImageView的layer设置contentsCenter后，拉伸的image也无效了！
+- (void)stretchDemo2 {
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(60, 200, 300, 80)];
+    imageView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:imageView];
+    
+    UIImage *image = [UIImage imageNamed:@"bg_fr_bubble_view"];
+    image = [image stretchableImageWithLeftCapWidth:image.size.width - 1 topCapHeight:3];
+    imageView.layer.contentsCenter = CGRectMake(0.2, 0.2, 0.1, 0.1);
+    imageView.image = image;
+}
+
+// 设置可视范围
+- (void)visualRectDemo {
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(60, 420, 100, 200);
+    layer.backgroundColor = [UIColor yellowColor].CGColor;
+    [self.view.layer addSublayer:layer];
+    
+    
+    UIImage *image = [UIImage imageNamed:@"IMG_1003.JPG"];
+    layer.contents = (id)image.CGImage;
+    layer.contentsGravity = kCAGravityResizeAspect;
+    layer.contentsScale = [UIScreen mainScreen].scale;
+    layer.contentsRect = CGRectMake(0, 0, 0.5, 0.5);
 }
 
 @end
