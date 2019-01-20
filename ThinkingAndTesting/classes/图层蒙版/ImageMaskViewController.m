e27c66d7bbbd9958783b168a6ee54ae25b856922
Dadong <yunluox@qq.com>
2019-01-20 11:14:13 +0800

    蒙版demo和一个事件分发实验

--- ThinkingAndTesting/classes/图层蒙版/ImageMaskViewController.m
+++ ThinkingAndTesting/classes/图层蒙版/ImageMaskViewController.m
@@ -17,9 +17,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor purpleColor] ;
-    [self way1] ;
+
+//    [self testTintImage];
+//    [CertificationUtils savedCertificatesOfUrl:@"https://dev.api.m.kachemama.com/"] ;
     
-    [CertificationUtils savedCertificatesOfUrl:@"https://dev.api.m.kachemama.com/"] ;
+    [self way1];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, DD_SCREEN_WIDTH, 60)];
+    label.backgroundColor = [UIColor whiteColor];
+    label.font = [UIFont systemFontOfSize:15];
+    [self.view addSubview:label];
+    label.text = @"的我";
+    label.adjustsFontSizeToFitWidth  = YES;
+    UIGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(a_test)];
+    [label addGestureRecognizer:tap];
+    label.userInteractionEnabled = YES;
+    
+    
+    ///
+    /// 实验说明: 在卡住主线程之后，点击某个可以响应的控件A，之后主线程从卡住中恢复，但是设置控件A为userInteractionEnabled=NO,这时候事件还
+    ///     可以传递麽？
+    ///         发现不行！如果没设置userInteractionEnabled=NO，是可以传递的！可以得出，事件传递真正是在主线中进行的！分发之后，还是要走原
+    ///      来的流程.
+    ///
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        DDLog(@"before sleeping");
+        [NSThread sleepForTimeInterval:5];
+        DDLog(@"after sleeping");
+
+        label.userInteractionEnabled = NO;
+        DDLog(@"complete setting");
+    });
+}
+
+- (void)a_test {
+    DDLog(@"a_test");
 }
 
 //
@@ -27,7 +59,11 @@
 //
 - (void)way1
 {
-    UIImage * i = [self maskImage:[UIImage imageNamed:@"image3"] withMask:[UIImage imageNamed:@"discount_star"] ] ;
+    UIImage *grayColorImage = [[UIImage imageNamed:@"mask2"] grayColorImage];
+    
+    UIImage *image = [[UIImage imageNamed:@"image3.jpeg"] maskWithImage:grayColorImage];
+    
+    UIImage * i = [self maskImage:[UIImage imageNamed:@"image3.jpeg"] withMask:[UIImage imageNamed:@"mask2"] ] ;
     
     UIImageView * imageV = [[UIImageView alloc] initWithImage:i] ;
     [self.view addSubview:imageV] ;
@@ -69,14 +105,28 @@
                                              );
     
 
-    
+    /* 第二个参数必须是用CGImageMaskCreate创建的遮罩图像，否则无效！*/
     CGImageRef maskedReference = CGImageCreateWithMask(imageReference, imageMask);
     CGImageRelease(imageMask);
     
-    UIImage *maskedImage = [UIImage imageWithCGImage:maskedReference];
+    UIImage *maskedImage = [UIImage imageWithCGImage:maskedReference scale:image.scale orientation:image.imageOrientation];
     CGImageRelease(maskedReference);
     
     return maskedImage;
+}
+
+- (void)testTintImage {
+    UIImage *image = [UIImage imageNamed:@"discount_star"];
+    UIImage *tintImage = [image imageWithTintColor:[UIColor greenColor]];
+
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    imageView.image = tintImage;
+    [self.view addSubview:imageView];
+    imageView.dd_top = 200;
+    imageView.dd_left = 100;
+    
+    UIImage *gradientImage = [image imageWithGradientTintColor:[UIColor greenColor]];
+    imageView.image = gradientImage;
 }
 
 @end
